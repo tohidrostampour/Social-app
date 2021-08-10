692daef445931b89699d24d4788ce572f02fc545
twhidrst <rostampourtwhid@gmail.com>
2021-08-10 20:15:52 +0430

    added post app and its fields

--- A/posts/models.py
+++ A/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE)
+    body = models.TextField()
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
