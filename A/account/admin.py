8a68c567d929da633e9e0452b6eac35455a1de7b
twhidrst <rostampourtwhid@gmail.com>
2021-08-18 18:32:40 +0430

    extended user model

--- A/account/admin.py
+++ A/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from account.models import Profile
 
-# Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class ExtendedProfileAdmin(admin.ModelAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User,ExtendedProfileAdmin)
