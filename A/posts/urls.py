b0df9f8c21682546b39c8e81e4705987adfd22b6
twhidrst <rostampourtwhid@gmail.com>
2021-08-18 23:12:05 +0430

    added adding post functionality

--- A/posts/urls.py
+++ A/posts/urls.py
@@ -5,5 +5,6 @@ from .views import *
 app_name = 'posts'
 urlpatterns = [
     path('',all_posts, name='all_posts'),
-    path('<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
+    path('<int:user_id>/<int:year>/<int:month>/<int:day>/<slug:slug>/', post_detail, name='post_detail'),
+    path('add_post/<int:user_id>/',add_post,name='add_post')
 ]
--- A/posts/views.py
+++ A/posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
+from .forms import AddPostForm
+from django.utils.text import slugify
+from django.contrib import messages
+
+
 
 
 def all_posts(request):
@@ -7,7 +12,22 @@ def all_posts(request):
     return render(request,'posts/all_posts.html', {'posts':posts})
 
 
-def post_detail(request, year, month, day, slug):
-    post = get_object_or_404(Post,created__year=year,created__month=month,
+def post_detail(request,user_id,year, month, day, slug):
+    post = get_object_or_404(Post,id=user_id,created__year=year,created__month=month,
         created__day=day,slug=slug)
     return render(request,'posts/post_detail.html',{'post':post})
+
+
+def add_post(request, user_id):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.user = request.user
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, "Post Added Successfully!", 'success')
+            return redirect('account:dashboard',user_id)
+    else:
+        form = AddPostForm()
+    return render(request, 'posts/add_post.html',{'form':form})
